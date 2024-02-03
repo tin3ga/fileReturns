9e4cefd7e644bfee7eaa820a130a453d99f5c67e
tin3ga <118301331+tin3ga@users.noreply.github.com>
2024-02-03 09:31:37 +0300

    Update webpage_handler.py

--- webpage_handler.py
+++ webpage_handler.py
@@ -5,23 +5,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import UnexpectedAlertPresentException
 
 from captcha_handler import CaptchaHandler
 
-load_dotenv("G:\My Drive\.env")
+load_dotenv()
 
 OPTIONS = webdriver.ChromeOptions()
 OPTIONS.add_experimental_option('detach', True)
+BROWSER_PATH = os.getenv('BROWSER_PATH')
+OPTIONS.binary_location = BROWSER_PATH
 
 URL = 'https://itax.kra.go.ke/KRA-Portal/'
 
-PIN = os.getenv('pin_KRA')
-PASSWORD = os.getenv('password_KRA')
+PIN = os.getenv('pin')
+PASSWORD = os.getenv('password')
 
 
 class WebPageHandler:
     @staticmethod
-    def login():
+    def fill_nil():
         driver = webdriver.Chrome(options=OPTIONS)
         driver.get(URL)
 
@@ -37,44 +40,77 @@ class WebPageHandler:
         continue_button.click()
 
         # wait for the password input to be displayed on page
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.XPATH,
                                             '//*[@id="xxZTT9p2wQ"]')))
 
         password_input = driver.find_element(By.ID, value='xxZTT9p2wQ')
         password_input.send_keys(PASSWORD)
 
+        # wait for the captcha input to be displayed on page
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH,
+                                            '// *[ @ id = "captcha_img"]')))
+
         # download captcha image, saves it in root directory
         with open('captcha.png', 'wb') as file:
             captcha = driver.find_element(By.XPATH, value='//*[@id="captcha_img"]').screenshot_as_png
             file.write(captcha)
-        captcha_ans = CaptchaHandler().process_captcha()
 
-        captcha_input = driver.find_element(By.XPATH, value='//*[@id="captcahText"]')
-        captcha_input.send_keys(captcha_ans)
+        try:
+            captcha_ans = CaptchaHandler().process_captcha()
+        except ValueError:
+            print("There was an error processing the captcha image. Try Again!")
+        else:
 
-        login_button = driver.find_element(By.XPATH, value='//*[@id="loginButton"]')
-        login_button.click()
-        returns_menu = driver.find_element(By.XPATH, value='//*[@id="ddtopmenubar"]/ul/li[3]/a')
+            captcha_input = driver.find_element(By.XPATH, value='//*[@id="captcahText"]')
 
-        actions = ActionChains(driver)
+            try:
+                captcha_input.send_keys(captcha_ans)
 
-        actions.move_to_element(returns_menu).perform()
-        file_nil = driver.find_element(By.XPATH, value='//*[@id="Returns"]/li[4]/a')
-        actions.move_to_element(file_nil).perform()
+            except TypeError:
 
-        file_nil.click()
+                print("There was an error processing the captcha image. Try Again!")
 
-        # click on tax obligation dropdown list
-        driver.find_element(By.XPATH, value='//*[@id="regType"]').click()
+            else:
 
-        # select resident individual
-        driver.find_element(By.XPATH, value='//*[@id="regType"]/option[2]').click()
+                login_button = driver.find_element(By.XPATH, value='//*[@id="loginButton"]')
+                login_button.click()
 
-        # click on next
-        driver.find_element(By.XPATH, value='//*[@id="btnSubmit"]').click()
+                # wait for the return menu to be displayed on page i.e verify a successful login
+                WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located((By.XPATH,
+                                                    '//*[@id="ddtopmenubar"]/ul/li[3]/a')))
+
+                returns_menu = driver.find_element(By.XPATH, value='//*[@id="ddtopmenubar"]/ul/li[3]/a')
+
+                actions = ActionChains(driver)
+
+                actions.move_to_element(returns_menu).perform()
+                file_nil = driver.find_element(By.XPATH, value='//*[@id="Returns"]/li[4]/a')
+                actions.move_to_element(file_nil).perform()
+
+                file_nil.click()
+
+                # click on tax obligation dropdown list
+                driver.find_element(By.XPATH, value='//*[@id="regType"]').click()
+
+                # select resident individual
+                driver.find_element(By.XPATH, value='//*[@id="regType"]/option[2]').click()
+
+                # click on next
+                driver.find_element(By.XPATH, value='// *[ @ id = "btnSubmit"]').click()
+
+                # click on submit
+                try:
+                    driver.find_element(By.XPATH, value='//*[@id="sbmt_btn"]').click()
+                except UnexpectedAlertPresentException as e:
+                    print(str(e.alert_text)) # print alert text if returns we already filed
+                else:
+                    # click ok to file a nil return
+                    driver.switch_to.alert.accept()
 
 
 if __name__ == '__main__':
     page = WebPageHandler()
-    page.login()
+    page.fill_nil()
